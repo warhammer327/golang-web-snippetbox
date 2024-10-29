17f58cb17b6e2b65f0409017357fef73e3ffd625
Syed Rizwan <rizwansm@outlook.com>
2024-10-29 21:43:15 +0600

    created db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,18 +16,23 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "http service address")
+	dsn := flag.String("dsn", "web:goof@/snippetbox?parseTime=true","MYSQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal()
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-
-
-
 
 	srv := &http.Server{
 		Addr:     *addr,
@@ -34,6 +41,17 @@ func main() {
 	}
 
 	infoLog.Printf("listening on %v", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string)(*sql.DB, error){
+	db, err := sql.Open("mysql",dsn)
+	if err!= nil{
+		return nil, err
+	}
+	if err = db.Ping(); err!=nil{
+		return nil, err
+	}
+	return db, nil
 }
