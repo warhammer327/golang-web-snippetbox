9dbe549a07095d963fff955b2fbc10743afc6631
Syed Rizwan <rizwansm@outlook.com>
2024-10-31 20:43:21 +0600

    completed multi row query

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,8 +2,8 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
 	"warhammer327.github.io/snippetbox/pkg/models"
-
 )
 
 type SnippetModel struct {
@@ -15,23 +15,57 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 	stmt := `INSERT INTO snippets (title, content, created, expires)
 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 	result, err := m.DB.Exec(stmt, title, content, expires)
-	if err != nil{
+	if err != nil {
 		return 0, err
 	}
 	id, err := result.LastInsertId()
-	if err != nil{
+	if err != nil {
 		return 0, err
 	}
 	return int(id), nil
 }
 
-
-
-
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows){
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	return nil, nil
+	stmt := `select id, title, content, created, expires from snippets
+	where expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 3`
+	
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	snippets := []*models.Snippet{}
+	for rows.Next(){
+		s := &models.Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets,s)
+	}
+	
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return snippets, nil
 }
