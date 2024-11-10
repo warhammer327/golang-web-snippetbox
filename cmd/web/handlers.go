9233d99ec32e2e5aeb95647ea1df1a387dff6a1d
Syed Rizwan <rizwansm@outlook.com>
2024-11-11 01:27:15 +0600

    Completed parsing errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -120,10 +120,10 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 	// If there are any errors, dump them in a plain text HTTP response and return
 	// from the handler.
-	if len(errors) > 0 {
-		fmt.Fprint(w, errors)
-		return
-	}
+	//if len(errors) > 0 {
+	//	fmt.Fprint(w, errors)
+	//	return
+	//}
 
 	if len(errors) > 0 {
 		app.render(w, r, "create.page.tmpl", &templateData{FormErrors: errors, FormData: r.Form})
